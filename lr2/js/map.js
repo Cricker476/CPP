937ff4aedb0f41bd60e05a13b267d918a47d6bb3
misha-lavrov <misha.lavrov1999@gmail.com>
2019-12-27 22:01:14 +0200

    just renamed variable

--- lr2/js/map.js
+++ lr2/js/map.js
@@ -1,7 +1,7 @@
 let map;
 let marker;
 let url = "https://api.openweathermap.org/data/2.5/weather?APPID=43ac0a3664a447dec6dd624247cb7d83";
-const CELSIUS_ZERO = 273.15
+const ABSOLUTE_ZERO = -273.15
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 49.2327805, lng: 28.4809704},
@@ -64,10 +64,10 @@ function createInfoWindowContent(response){
         </div>
         <div id="temperature">
             <h2>Temperature:</h2>
-            Real: ${Math.round((response.main.temp - CELSIUS_ZERO) * 10) / 10} C
-            <br>Feels like: ${Math.round((response.main.feels_like - CELSIUS_ZERO) * 10) / 10} C
-            <br>Max: ${Math.round((response.main.temp_max - CELSIUS_ZERO) * 10) / 10} C
-            <br>Min: ${Math.round((response.main.temp_min - CELSIUS_ZERO) * 10) / 10} C
+            Real: ${Math.round((response.main.temp + ABSOLUTE_ZERO) * 10) / 10} C
+            <br>Feels like: ${Math.round((response.main.feels_like + ABSOLUTE_ZERO) * 10) / 10} C
+            <br>Max: ${Math.round((response.main.temp_max + ABSOLUTE_ZERO) * 10) / 10} C
+            <br>Min: ${Math.round((response.main.temp_min + ABSOLUTE_ZERO) * 10) / 10} C
         </div>
         <div id="weather">
             <h2>The weather is: ${response.weather[0].main}</h2>
